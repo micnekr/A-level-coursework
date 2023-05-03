1ca5c729f5db773bdc53f4d6ce2256a4c94c06d2
micnekr <misha.nekrasov@gmail.com>
2023-05-03 21:18:38 +0100

    Added more comments

--- public/jsx/components/PasswordStrength.jsx
+++ public/jsx/components/PasswordStrength.jsx
@@ -2,55 +2,55 @@
  * displays a progress bar that represents the password strength
  */
 function PasswordStrength(props) {
-  const { password} = props;
+    const { password } = props;
 
-  // Do not display anything on empty passwords
-  if (password === "") return null;
+    // Do not display anything on empty passwords
+    if (password === "") return null;
 
-  // Bar colours based on password strength
-  const bar_colors = [
-    "#F71735", // red
-    "#FF7700", // orange
-    "#FF7700", // orange
-    "#A1E887", // green
-    "#A1E887", // green
-  ];
+    // Bar colours based on password strength
+    const bar_colors = [
+        "#F71735", // red
+        "#FF7700", // orange
+        "#FF7700", // orange
+        "#A1E887", // green
+        "#A1E887", // green
+    ];
 
-  // Analyse the password strength
-  const password_analysis_result = zxcvbn(password);
+    // Analyse the password strength
+    const password_analysis_result = zxcvbn(password);
 
-  // the score goes from 0 to 4
-  const password_strength = password_analysis_result.score;
-  const password_strength_message = password_analysis_result.feedback.warning;
+    // the score goes from 0 to 4
+    const password_strength = password_analysis_result.score;
+    const password_strength_message = password_analysis_result.feedback.warning;
 
-  const bar_color = bar_colors[password_strength];
+    const bar_color = bar_colors[password_strength];
 
-  const total_bar_length = 5;
-  // Always show a little progress. Note that password strength varies between 0 and 4 (inclusive)
-  const length_highlighted = password_strength + 1;
+    const total_bar_length = 5;
+    // Always show a little progress. Note that password strength varies between 0 and 4 (inclusive)
+    const length_highlighted = password_strength + 1;
 
-  return <div className="container-fluid">
+    return <div className="container-fluid">
 
-           <div className="row mb-3">
-             <div className="col-auto ps-0 mb-2 mb-sm-0">Password strength: </div>
-             {/* The "outside" of the bar */}
-             <div className="col-sm rounded-pill border px-0 mx-auto align-self-center" style={{
-               height: "1rem"
-             }}>
+        <div className="row mb-3">
+            <div className="col-auto ps-0 mb-2 mb-sm-0">Password strength: </div>
+            {/* The "outside" of the bar */}
+            <div className="col-sm rounded-pill border px-0 mx-auto align-self-center" style={{
+                height: "1rem"
+            }}>
                 {/* The colourful "inside" of the bar */}
                 <div className="h-100 rounded-pill" style={{
                     backgroundColor: bar_color,
                     // Calculate percentage width
                     width: `${length_highlighted / total_bar_length * 100}%`
                 }}></div>
-             </div>
-           </div>
-            {
-                // if it an empty string, do not display it
-            password_strength_message === "" ? null:
+            </div>
+        </div>
+        {
+            // if it an empty string, do not display it
+            password_strength_message === "" ? null :
                 <div className="text-center mb-3">
-                Warning: {password_strength_message}
+                    Warning: {password_strength_message}
                 </div>
-            }
-  </div>
+        }
+    </div>
 }
--- public/jsx/pages/Login.jsx
+++ public/jsx/pages/Login.jsx
@@ -1,3 +1,4 @@
+// Imports
 const Button = ReactBootstrap.Button;
 const Form = ReactBootstrap.Form;
 
@@ -5,61 +6,65 @@ const useState = React.useState;
 const useEffect = React.useEffect;
 
 function Login() {
-  // Have variables to keep track of the state
-  const [username, set_username] = useState("");
-  const [password, set_password] = useState("");
+    // Have variables to keep track of the state
+    const [username, set_username] = useState("");
+    const [password, set_password] = useState("");
 
-  // em is short for "error message"
-  const [username_em, set_username_em] = useState("");
-  const [password_em, set_password_em] = useState("");
-  const [overall_em, set_overall_em] = useState("");
+    // em is short for "error message"
+    const [username_em, set_username_em] = useState("");
+    const [password_em, set_password_em] = useState("");
+    const [overall_em, set_overall_em] = useState("");
 
-  // Remove the error messages when typing
-  useEffect(() => {
-    set_username_em("");
-    set_password_em("");
-    set_overall_em("");
-  }, [username, password]);
+    // Remove the error messages when typing
+    useEffect(() => {
+        set_username_em("");
+        set_password_em("");
+        set_overall_em("");
+    }, [username, password]);
 
-  return <PageContainerBox title="Log in">
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control type="text" placeholder="Username" value={username} onChange={e => set_username(e.target.value)} />
-        <ErrorMessage em={username_em} />
-      </Form.Group>
+    return <PageContainerBox title="Log in">
+        <Form>
+          // The username input
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Username" value={username} onChange={e => set_username(e.target.value)} />
+                <ErrorMessage em={username_em} />
+            </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" value={password} onChange={e => set_password(e.target.value)} />
-        <ErrorMessage em={password_em} />
-      </Form.Group>
-      <ErrorMessage em={overall_em} />
-      <Button variant="primary" onClick={submit}>
-        Log in
-      </Button>
-    </Form>
-  </PageContainerBox>;
+          // The password input
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" value={password} onChange={e => set_password(e.target.value)} />
+                <ErrorMessage em={password_em} />
+            </Form.Group>
+          // This error message shows up if there is an error message received from the server
+            <ErrorMessage em={overall_em} />
+          // Submit button
+            <Button variant="primary" onClick={submit}>
+                Log in
+            </Button>
+        </Form>
+    </PageContainerBox>;
 
 
-  async function submit() {
-    // Check that a username and a password were supplied
-    if (username === "") return set_username_em("Please specify a username");
-    if (password === "") return set_password_em("Please specify a password");
-    // Make the actual request
-    const res = await f("/api/login", "POST", {
-      username, password
-    });
+    async function submit() {
+        // Check that a username and a password were supplied
+        if (username === "") return set_username_em("Please specify a username");
+        if (password === "") return set_password_em("Please specify a password");
+        // Make the actual request
+        const res = await f("/api/login", "POST", {
+            username, password
+        });
 
-    // if it was not successful, show the error message
-    if (res.status >= 400) {
-      // Read the error message
-      const error = await res.text();
-      return set_overall_em(error);
+        // if it was not successful, show the error message
+        if (res.status >= 400) {
+            // Read the error message
+            const error = await res.text();
+            return set_overall_em(error);
+        }
+
+
+        // Otherwise, refirect to the calendar page
+        window.location.href = "/calendar";
     }
-
-
-    // Otherwise, refirect to the calendar page
-    window.location.href = "/calendar";
-  }
 }
